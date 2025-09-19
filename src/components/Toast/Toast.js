ac9f3c4f37937eed84dcee437883627fc1418a66
Callum Whitfield <callum.whitfield@gearset.com>
2025-09-19 14:52:31 +0100

    Remove `isOpen` prop

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -20,22 +20,20 @@ const ICONS_BY_VARIANT = {
   error: AlertOctagon,
 };
 
-function Toast({ variant, children, isOpen, close }) {
+function Toast({ variant, children, close }) {
   const Icon = ICONS_BY_VARIANT[variant];
 
   return (
-    isOpen && (
-      <div className={`${styles.toast} ${styles[variant]}`}>
-        <div className={styles.iconContainer}>
-          <Icon size={24} />
-        </div>
-        <p className={styles.content}>{children}</p>
-        <button className={styles.closeButton} onClick={close}>
-          <X size={24} />
-          <VisuallyHidden>Dismiss message</VisuallyHidden>
-        </button>
+    <div className={`${styles.toast} ${styles[variant]}`}>
+      <div className={styles.iconContainer}>
+        <Icon size={24} />
       </div>
-    )
+      <p className={styles.content}>{children}</p>
+      <button className={styles.closeButton} onClick={close}>
+        <X size={24} />
+        <VisuallyHidden>Dismiss message</VisuallyHidden>
+      </button>
+    </div>
   );
 }
 
